e42f5afdac85748c5dc1ed2a810fcb971a8131ee
Guy Umbright <gumbright@gmail.com>
2013-11-12 20:47:54 -0600

    Initial Commit

---
+++ UIXTagsControllerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UIXTagsControllerDemo
+//
+//  Created by Guy Umbright on 11/12/13.
+//  Copyright (c) 2013 Umbright Consulting, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIXTagsControllerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIXTagsControllerDemo
+//
+//  Created by Guy Umbright on 11/12/13.
+//  Copyright (c) 2013 Umbright Consulting, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
